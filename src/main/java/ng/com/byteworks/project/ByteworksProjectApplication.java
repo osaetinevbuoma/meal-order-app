93b9142c860eb2053d3f4911fac91dc8492141d0
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2020-01-23 16:21:12 +0100

    Initial commit

---
+++ src/main/java/ng/com/byteworks/project/ByteworksProjectApplication.java
@@ -0,0 +1,13 @@
+package ng.com.byteworks.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteworksProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteworksProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ng/com/byteworks/project/ByteworksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.com.byteworks.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteworksProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
